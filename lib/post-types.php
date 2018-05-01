e1d659cb70a32dc643d7eca8ea37011593207f30
Cas <cas@interglobal.vision>
2018-04-30 19:02:57 -0500

    add Product post type

--- lib/post-types.php
+++ lib/post-types.php
@@ -8,12 +8,60 @@ function add_menu_icons_styles(){
 #menu-posts-stockist .dashicons-admin-post:before {
     content: '\f513';
 }
+
+#menu-posts-product .dashicons-admin-post:before {
+  content: "\f174";
+}
 </style>
 
 <?php
 }
 add_action( 'admin_head', 'add_menu_icons_styles' );
 
+
+//Register Product Post Types
+add_action( 'init', 'register_cpt_product' );
+
+function register_cpt_product() {
+
+  $labels = array(
+    'name' => _x( 'Products', 'product' ),
+    'singular_name' => _x( 'Product', 'product' ),
+    'add_new' => _x( 'Add New', 'product' ),
+    'add_new_item' => _x( 'Add New Product', 'product' ),
+    'edit_item' => _x( 'Edit Product', 'product' ),
+    'new_item' => _x( 'New Product', 'product' ),
+    'view_item' => _x( 'View Product', 'product' ),
+    'search_items' => _x( 'Search Products', 'product' ),
+    'not_found' => _x( 'No products found', 'product' ),
+    'not_found_in_trash' => _x( 'No products found in Trash', 'product' ),
+    'parent_item_colon' => _x( 'Parent Product:', 'product' ),
+    'menu_name' => _x( 'Products', 'product' ),
+  );
+
+  $args = array(
+    'labels' => $labels,
+    'hierarchical' => false,
+
+    'supports' => array( 'title', 'editor', 'thumbnail' ),
+
+    'public' => true,
+    'show_ui' => true,
+    'show_in_menu' => true,
+    'menu_position' => 5,
+
+    'show_in_nav_menus' => true,
+    'publicly_queryable' => true,
+    'exclude_from_search' => false,
+    'has_archive' => true,
+    'query_var' => true,
+    'can_export' => true,
+    'rewrite' => true,
+    'capability_type' => 'post'
+  );
+
+  register_post_type( 'product', $args );
+}
 
 //Register Custom Post Types
 add_action( 'init', 'register_cpt_stockist' );
