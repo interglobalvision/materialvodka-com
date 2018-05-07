d72fbf7168870713a82c1fd803f6790a5d7f0423
neener <janinemrosen@gmail.com>
2018-05-07 17:51:45 -0400

    fixing indent

--- lib/functions-taxonomies.php
+++ lib/functions-taxonomies.php
@@ -1,13 +1,13 @@
 <?php
 
-function season_taxonomy() {  
-	register_taxonomy(  
+function season_taxonomy() {
+  register_taxonomy(  
     'season',  //The name of the taxonomy. Name should be in slug form (must not contain capital letters or spaces). 
     'recipe',  //post type name
     array(  
-        'hierarchical' => true,  
-        'label' => 'Seasons',  //Display name
-        'query_var' => true,
+      'hierarchical' => true,  
+      'label' => 'Seasons',  
+      'query_var' => true,
     )  
 	);  
 }  
