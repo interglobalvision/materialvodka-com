93cb409280488b786bee316f2c21ca8ee384019e
Cas <cas@interglobal.vision>
2018-05-07 14:11:06 -0500

    add Locate page metaboxes

--- lib/meta-boxes.php
+++ lib/meta-boxes.php
@@ -40,4 +40,158 @@ function igv_cmb_metaboxes() {
    */
 
 }
+/**
+ * Add Locate page metaboxes
+ */
+add_action( 'cmb2_init', 'igv_cmb_metaboxes_locate_page' );
+function igv_cmb_metaboxes_locate_page() {
+
+  $prefix = '_igv_';
+
+  // Get Locate page
+  $locate_page = get_page_by_path('/locate');
+
+  // Check if locate page exists
+  if(!empty($locate_page)) {
+
+    // Init metabox
+    $locate_meta = new_cmb2_box( array(
+      'id'            => $prefix . 'locate_options',
+      'title'         => __( 'Options', 'cmb2' ),
+      'object_types'  => array( 'page', ), // Post type
+      'show_on'       => array(
+        'key' => 'id',
+        'value' => array($locate_page->ID)
+      ),
+      'context'       => 'normal',
+      'priority'      => 'high',
+      'show_names'    => true, // Show field names on the left
+      // 'cmb_styles' => false, // false to disable the CMB stylesheet
+      // 'closed'     => true, // Keep the metabox closed by default
+    ) );
+
+
+    // Home Image
+    $locate_meta->add_field( array(
+      'name'    => 'Home Image',
+      'id'      => $prefix . 'locate_home_image',
+      'type'    => 'file',
+      // Optional:
+      'options' => array(
+        'url' => false, // Hide the text input for the url
+      ),
+      // query_args are passed to wp.media's library query.
+      'query_args' => array(
+        'type' => array(
+          'image/jpeg',
+          'image/png',
+        ),
+      ),
+      'preview_size' => 'large', // Image size to use when previewing in the admin.
+    ) );
+
+    // Top Image
+    $locate_meta->add_field( array(
+      'name'    => 'Top Image',
+      'id'      => $prefix . 'locate_top_image',
+      'type'    => 'file',
+      // Optional:
+      'options' => array(
+        'url' => false, // Hide the text input for the url
+      ),
+      // query_args are passed to wp.media's library query.
+      'query_args' => array(
+        'type' => array(
+          'image/jpeg',
+          'image/png',
+        ),
+      ),
+      'preview_size' => 'large', // Image size to use when previewing in the admin.
+    ) );
+
+    // Bottom Image
+    $locate_meta->add_field( array(
+      'name'    => 'Bottom Image',
+      'id'      => $prefix . 'locate_bottom_image',
+      'type'    => 'file',
+      // Optional:
+      'options' => array(
+        'url' => false, // Hide the text input for the url
+      ),
+      // query_args are passed to wp.media's library query.
+      'query_args' => array(
+        'type' => array(
+          'image/jpeg',
+          'image/png',
+        ),
+      ),
+      'preview_size' => 'large', // Image size to use when previewing in the admin.
+    ) );
+
+
+    // Video group
+    $locate_video_id = $locate_meta->add_field( array(
+      'id'      => $prefix . 'locate_video_group',
+      'name'    => 'Video',
+      'type'        => 'group',
+      'repeatable'  => false, // use false if you want non-repeatable group
+    ) );
+
+    // Video mp4
+    $locate_meta->add_group_field( $locate_video_id, array(
+      'name'    => 'Video (mp4)',
+      'id'      => $prefix . 'locate_video_mp4',
+      'type'    => 'file',
+      // Optional:
+      'options' => array(
+        'url' => false, // Hide the text input for the url
+      ),
+      // query_args are passed to wp.media's library query.
+      'query_args' => array(
+        'type' => array(
+          'video/mp4',
+        ),
+      ),
+    ) );
+
+    // Video webm
+    $locate_meta->add_group_field( $locate_video_id, array(
+      'name'    => 'Video (webm)',
+      'id'      => $prefix . 'locate_video_webm',
+      'type'    => 'file',
+      // Optional:
+      'options' => array(
+        'url' => false, // Hide the text input for the url
+      ),
+      // query_args are passed to wp.media's library query.
+      'query_args' => array(
+        'type' => array(
+          'video/webm',
+        ),
+      ),
+    ) );
+
+    // Video webm
+    $locate_meta->add_group_field( $locate_video_id, array(
+      'name'    => 'Video Poster',
+      'id'      => $prefix . 'locate_video_poster',
+      'type'    => 'file',
+      // Optional:
+      'options' => array(
+        'url' => false, // Hide the text input for the url
+      ),
+      // query_args are passed to wp.media's library query.
+      'query_args' => array(
+        'type' => array(
+          'image/jpeg',
+          'image/png',
+        ),
+      ),
+      'preview_size' => 'large', // Image size to use when previewing in the admin.
+    ));
+  }
+
+  //
+
+}
 ?>
