c1f4a4a206c22396cb0933f7776dceef24e0ef55
neener <janinemrosen@gmail.com>
2018-05-07 16:10:44 -0400

    add video awards post type

--- lib/post-types.php
+++ lib/post-types.php
@@ -58,3 +58,46 @@ function register_cpt_stockist() {
 
   register_post_type( 'stockist', $args );
 }
+
+add_action( 'init', 'register_cpt_video_award' );
+
+function register_cpt_video_award() {
+
+  $labels = array(
+    'name' => _x( 'Video Awards', 'video_award' ),
+    'singular_name' => _x( 'Video Award', 'video_award' ),
+    'add_new' => _x( 'Add New', 'video_award' ),
+    'add_new_item' => _x( 'Add New Video Award', 'video_award' ),
+    'edit_item' => _x( 'Edit Video Award', 'video_award' ),
+    'new_item' => _x( 'New Video Award', 'video_award' ),
+    'view_item' => _x( 'View Video Award', 'video_award' ),
+    'search_items' => _x( 'Search Video Awards', 'video_award' ),
+    'not_found' => _x( 'No video awards found', 'video_award' ),
+    'not_found_in_trash' => _x( 'No video awards found in Trash', 'video_award' ),
+    'parent_item_colon' => _x( 'Parent Video Award:', 'video_award' ),
+    'menu_name' => _x( 'Video Awards', 'video_award' ),
+  );
+
+  $args = array(
+    'labels' => $labels,
+    'hierarchical' => false,
+
+    'supports' => array( 'title', 'editor', 'thumbnail' ),
+
+    'public' => true,
+    'show_ui' => true,
+    'show_in_menu' => true,
+    'menu_position' => 5,
+
+    'show_in_nav_menus' => true,
+    'publicly_queryable' => true,
+    'exclude_from_search' => false,
+    'has_archive' => true,
+    'query_var' => true,
+    'can_export' => true,
+    'rewrite' => true,
+    'capability_type' => 'post'
+  );
+
+  register_post_type( 'video_award', $args );
+}
