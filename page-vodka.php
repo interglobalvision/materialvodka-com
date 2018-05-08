94eded31c1ca4eaf606f673a96302b576454da25
neener <janinemrosen@gmail.com>
2018-05-08 14:11:27 -0400

    metadata on page and add grid column classes

--- page-vodka.php
+++ page-vodka.php
@@ -4,8 +4,8 @@ get_header();
 
 <main id="main-content">
   <section id="">
-    <div class="container">
-      
+    <div <?php post_class('container'); ?> id="post-<?php the_ID(); ?>">
+    
 
 <?php
 if (have_posts()) {
@@ -25,42 +25,55 @@ if (have_posts()) {
     $world_image = get_post_meta(get_the_ID(), '_igv_world_image', true);
     pr($world_image);
 
-    $institution_list = get_post_meta(get_the_ID(), '_igv_institution_list_options', true);
+    $institution_list = get_post_meta(get_the_ID(), '_igv_institution_list_textarea', true);
     pr($institution_list);
 
 ?>
-
-      <video autoplay loop muted poster="<?php echo $vodka_video['poster']; ?>">
+    <div class="grid-row">
+      <div class="grid-item item-s-12">
+        <video autoplay loop muted poster="<?php echo $vodka_video['poster']; ?>">
           <source src="<?php echo $video['_igv_vodka_video_webm']; ?>" type="video/webm">
           <source src="<?php echo $video['_igv_vodka_video_mp4']; ?>" type="video/webm">
-      </video>
-
-      <h1 class='font-uppercase'>Material Vodka</h1>
-
-      <?php echo get_post_field('post_content', $post->ID); ?>
-
-      <div class="grid-item item-s-12 item-l-4" style="background-image: url(<?php echo $vodka_image; ?>)">
-
-      <video autoplay loop muted poster="<?php echo $world_video['poster']; ?>">
+        </video>
+      </div>
+    </div>
+    
+    <div class="grid-row">
+      <div class="grid-item item-s-12">
+        <h1 class='font-uppercase'>Material Vodka</h1>
+      </div>
+    </div>
+    
+    <div class="grid-row">
+      <div class="grid-item item-s-12 item-l-3">
+        <?php the_content(); ?>
+      </div>
+      <div class="grid-item item-s-12 item-l-9" style="background-image: url(<?php echo $vodka_image; ?>)"></div>
+    </div>
+    
+    <div class="grid-row">
+      <div class="grid-item item-s-12">
+        <video autoplay loop muted poster="<?php echo $world_video['poster']; ?>">
           <source src="<?php echo $video['_igv_world_video_webm']; ?>" type="video/webm">
           <source src="<?php echo $video['_igv_world_video_mp4']; ?>" type="video/webm">
-      </video>
+        </video>
+      </div>
+    </div>
+  
+    <div class="grid-row">
+      <div class="grid-item item-s-12 item-l-6">
+        <h1 class='font-uppercase'>10% OF THE PROFITS GO TO SUPPORTING THE ARTS.</h1>
 
-      <h1 class='font-uppercase'>10% OF THE PROFITS GO TO SUPPORTING THE ARTS.</h1>
+        <p>Material is woven into the framework of the creative community. We are members of our world and we actively support radical change.</p>
 
-      <p>Material is woven into the framework of the creative community. We are members of our world and we actively support radical change.</p>
+        <p>Some of the artists and cultural institutions we have worked with include:</p>
 
-      <p>Some of the artists and cultural institutions we have worked with include:</p>
+        <?php echo ( $institution_list ); ?>
 
-      <div class="grid-item item-s-12 item-l-4" style="background-image: url(<?php echo $world_image; ?>)">
+        <div class="grid-item item-s-12 item-l-4" style="background-image: url(<?php echo $world_image; ?>)"></div>
+      </div>
+    </div>
 
-      <article <?php post_class('grid-item item-s-12'); ?> id="post-<?php the_ID(); ?>">
-
-        <a href="<?php the_permalink() ?>"><?php the_title(); ?></a>
-
-        <?php the_content(); ?>
-
-      </article>
 
 <?php
   }
