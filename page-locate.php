cf968a0cbfa54e35b6f1b9bccb4505aad9efaa82
Cas <cas@interglobal.vision>
2018-05-07 21:44:09 -0500

    add page-locate template with zip code form and list of stockists

--- page-locate.php
+++ page-locate.php
@@ -3,33 +3,88 @@ get_header();
 ?>
 
 <main id="main-content">
-  <section id="">
+  <section id="locate">
     <div class="container">
-      <div class="grid-row">
 
 <?php
 if (have_posts()) {
   while (have_posts()) {
     the_post();
+
+    $top_image = get_post_meta(get_the_ID(), '_igv_locate_top_image', true);
+    $video = get_post_meta(get_the_ID(), '_igv_locate_video_group', true);
+    $video = $video[0];
+    $bottom_image = get_post_meta(get_the_ID(), '_igv_locate_bottom_image', true);
+
 ?>
+      <div class="grid-row">
+        <div class="grid-item item-s-12 item-l-8 no-gutter grid-row">
+          <div class="grid-item item-s-12">
+            <h1 class="font-uppercase">Find it near you</h1>
+          </div>
+          <div class="grid-item item-s-12 item-l-6">
+            <?php get_template_part('partials/zip-code-form'); ?>
+          </div>
+          <div class="grid-item item-s-12 item-l-6">
+          </div>
+        </div>
+        <div class="grid-item item-s-12 item-l-4" style="background-image: url(<?php echo $top_image; ?>)">
+        </div>
+      </div>
 
-        <article <?php post_class('grid-item item-s-12'); ?> id="post-<?php the_ID(); ?>">
+      <div class="grid-row">
+        <div class="grid-item item-s-12">
+          <video autoplay loop muted poster="<?php echo $video['_igv_locate_video_poster']; ?>">
+            <source src="<?php echo $video['_igv_locate_video_webm']; ?>" type="video/webm">
+            <source src="<?php echo $video['_igv_locate_video_mp4']; ?>" type="video/webm">
+          </video>
+        </div>
+      </div>
 
-          <a href="<?php the_permalink() ?>"><?php the_title(); ?></a>
+      <div class="grid-row">
+        <div class="grid-item item-s-12">
+          <h2>Ask for material</h2>
+        </div>
 
-          <?php the_content(); ?>
+<?php
+    // NY
+    $ny_query = get_stockists_by_city('ny');
+?>
+        <div class="grid-item item-s-12 item-l-6">
+          <h2>NY</h2>
+           <?php generate_stockists_list($ny_query); ?>
+        </div>
 
-        </article>
+<?php
+    // LA
+    $la_query = get_stockists_by_city('la');
+?>
+        <div class="grid-item item-s-12 item-l-3">
+          <h2>LA</h2>
+           <?php generate_stockists_list($la_query); ?>
+        </div>
 
+<?php
+    // MIAMI
+    $miami_query = get_stockists_by_city('miami');
+?>
+        <div class="grid-item item-s-12 item-l-3">
+          <h2>Miami</h2>
+           <?php generate_stockists_list($miami_query); ?>
+        </div>
+      </div>
+
+      <div class="grid-row">
+        <div class="grid-item item-s-12 item-l-6" style="background-image: url(<?php echo $bottom_image; ?>)">
+        </div>
+        <div class="grid-item item-s-12 item-l-6">
+        </div>
+      </div>
 <?php
   }
-} else {
+}
 ?>
-        <article class="u-alert grid-item item-s-12"><?php _e('Sorry, no posts matched your criteria :{'); ?></article>
-<?php
-} ?>
 
-      </div>
     </div>
   </section>
 
