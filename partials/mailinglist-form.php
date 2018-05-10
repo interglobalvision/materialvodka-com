06b4a4234debfac59505c4ae77732828d7b1be77
mikeyrayvon <mikey@interglobal.vision>
2018-05-09 19:50:36 -0500

    improve form markup

--- partials/mailinglist-form.php
+++ partials/mailinglist-form.php
@@ -1,9 +1,18 @@
 <?php
-  $options = get_site_option('_igv_site_options');
+$options = get_site_option('_igv_site_options');
+
+if (!empty($options['mailchimp_url'])) {
 ?>
 <div id="mailinglist-form-holder">
   <div>Enter your email address and stay informed</div>
-  <div class="grid-row">
-    <?php echo $options['_igv_mailinglist_embed']; ?>
-  </div>
-</form>
+  <form id="mailchimp-form" class="grid-row" novalidate="true">
+    <input id="mailchimp-email" type="email" placeholder="EMAIL" name="EMAIL" class="flex-grow">
+    <div class="grid-item">
+      <button type="submit">ENTER</button>
+    </div>
+    <div id="mailchimp-response" class="grid-item no-gutter item-s-12 margin-top-tiny">&nbsp;</div>
+  </form>
+</div>
+<?php
+}
+?>
