4fa5622b7aa153e0457e844b8baf6f5f47b4dfc7
Cas <cas@interglobal.vision>
2018-05-09 20:59:06 -0500

    add search stockists by zip and return json

--- lib/functions-api.php
+++ lib/functions-api.php
@@ -18,7 +18,42 @@ function find_stockists_call($data) {
   // Requestes all zipcodes whithin 3 miles
   $res = $client->request('GET', 'https://www.zipcodeapi.com/rest/' . $key . '/radius.json/' . $zipcode . '/3/mile');
 
-  return json_decode($res->getBody());
+  $decoded_res = json_decode($res->getBody());
+
+  $zip_codes = $decoded_res->zip_codes;
+
+  $query_zip_codes = [];
+
+  foreach($zip_codes as $zipcode) {
+    array_push($query_zip_codes, $zipcode->zip_code);
+  }
+
+  $args = array(
+    'post_type' => 'stockist',
+    'meta_query' => array(
+      array(
+        'key'     => '_igv_stockist_zipcode',
+        'value'   => $query_zip_codes,
+      ),
+    ),
+    'meta_key'     => '_igv_stockist_zipcode',
+    'posts_per_page' => -1,
+  );
+
+  $stockists = get_posts($args);
+
+
+  $stockists_with_meta = [];
+
+  foreach($stockists as $stockist) {
+    $meta = get_post_meta($stockist->ID);
+    $stockist_with_meta = array_merge($stockist->to_array(), $meta);
+
+    array_push($stockists_with_meta, $stockist_with_meta);
+  }
+
+  return $stockists_with_meta;
+
 }
 
 add_action( 'rest_api_init', function () {
