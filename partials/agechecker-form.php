31ed902da305af495034a38fc2c1fc9fbe0ef7dd
neener <janinemrosen@gmail.com>
2018-05-10 20:18:32 -0400

    remove container-overlay

--- partials/agechecker-form.php
+++ partials/agechecker-form.php
@@ -1,36 +1,32 @@
-<div id="age-overlay">
+<div id="age-overlay grid-row justify-center">
 
-  <div class="container-overlay grid-row justify-center">
+  <form id="age-checker-form" class="grid-column align-items-center justify-center grid-item item-s-4">
+    
+    <div class="padding-bottom-basic">
+      <?php get_template_part('partials/logo') ?></a>
+    </div>
 
-    <form id="age-checker-form" class="grid-column align-items-center justify-center grid-item item-s-4">
-      
-      <div class="padding-bottom-basic">
-        <?php get_template_part('partials/logo') ?></a>
+    <p>Please enter your date of birth:<p>
+    
+    <div class="grid-row justify-center padding-bottom-small">
+      <div>
+        <input id="birthday-month" name="month" value="" type="number" min="1" max="12" placeholder="MM">
       </div>
 
-      <p>Please enter your date of birth:<p>
-      
-      <div class="grid-row justify-center padding-bottom-small">
-        <div>
-          <input id="birthday-month" name="month" value="" type="number" min="1" max="12" placeholder="MM">
-        </div>
-
-        <div>
-           <input id="birthday-day" name="day" value="" type="number" min="1" max="31" placeholder="DD">
-        </div>
-
-        <div>
-           <input id="birthday-year" name="year" value="" type="number" min="1800" max="<?php echo date("Y"); ?>"placeholder="YYYY">
-        </div>
+      <div>
+         <input id="birthday-day" name="day" value="" type="number" min="1" max="31" placeholder="DD">
       </div>
 
-      <p>You must be of legal age to enter<p>
+      <div>
+         <input id="birthday-year" name="year" value="" type="number" min="1800" max="<?php echo date("Y"); ?>"placeholder="YYYY">
+      </div>
+    </div>
 
-      <button id="submit-age">Enter</button>
+    <p>You must be of legal age to enter<p>
 
-    </form>
+    <button id="submit-age">Enter</button>
 
-  </div>
+  </form>
 
 </div>
 
