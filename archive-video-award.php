c8f21a0ba14aa3376ac39d910178bdf600206fc4
neener <janinemrosen@gmail.com>
2018-05-11 13:08:38 -0400

    getting unique artist and dates for else loops for grid videos

--- archive-video-award.php
+++ archive-video-award.php
@@ -12,28 +12,28 @@ if (have_posts()) {
 
   while (have_posts()) {
     the_post();
-
-    if ($wp_query->current_post == 0) {
     $video_year = get_post_meta($post->ID, '_igv_video_award_video_year', true);
     $artists = get_post_meta($post->ID, '_igv_video_award_artists', true);
     $bio = get_post_meta($post->ID, '_igv_video_award_bio', true);
     $minutes = get_post_meta($post->ID, '_igv_video_award_minutes', true);
     $seconds = get_post_meta($post->ID, '_igv_video_award_seconds', true);
-    
+
+    if ($wp_query->current_post == 0) {
+        
 ?>
     <div class="information-overlay item item-m-4">
-      <h2 class="font-uppercase"><?php the_title(); ?></h2>
-      <div><?php echo ($video_year); ?></div>
-      <div><?php echo ($minutes); ?> minutes <?php echo ($seconds); ?> seconds</div>
+      <h2 class="font-uppercase font-size-large"><?php the_title(); ?></h2>
+      <div><?php echo $video_year; ?></div>
+      <div><?php echo $minutes; ?> minutes <?php echo $seconds; ?> seconds</div>
       <p><?php the_content(); ?>
-      <h2 class="font-uppercase"><?php echo ($artists); ?></h2>
-      <p><?php echo ($bio); ?></p>
+      <h2 class="font-uppercase"><?php echo $artists; ?></h2>
+      <p><?php echo $bio; ?></p>
     </div>
 
     <div class="grid-item item-s-12 background-cover font-uppercase" style="background-image: url(<?php echo the_post_thumbnail_url('full'); ?>)">
       <h2><?php the_title(); ?></h2>
-      <div><?php echo ( $artists ); ?></div> 
-      <div><?php the_date('F Y'); ?></div>
+      <div><?php echo $artists; ?></div> 
+      <div><?php echo get_the_date('F Y'); ?></div>
       <div class="font-uppercase">information</div>
     </div>
   
@@ -43,8 +43,8 @@ if (have_posts()) {
   
     <div class="grid-item item-s-4 background-cover font-uppercase" style="background-image: url(<?php echo the_post_thumbnail_url('full'); ?>)">
       <h2><?php the_title(); ?></h2>
-      <div><?php echo ( $artists ); ?></div> 
-      <div><?php the_date('F Y'); ?></div>
+      <div><?php echo $artists; ?></div>
+      <div><?php echo get_the_date('F Y'); ?></div>
       <div class="font-uppercase">information</div>
     </div>
   
