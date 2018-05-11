f1c5257f98ddd911827a9b638e06df500b85e1ef
Cas <cas@interglobal.vision>
2018-05-11 14:34:37 -0500

    fix indentation

--- archive-video-award.php
+++ archive-video-award.php
@@ -12,15 +12,15 @@ if (have_posts()) {
 
   while (have_posts()) {
     the_post();
-    
+
     $artists = get_post_meta($post->ID, '_igv_video_award_artists', true);
-    
+
     if ($wp_query->current_post == 0) {
-    $video_year = get_post_meta($post->ID, '_igv_video_award_video_year', true);
-    $bio = get_post_meta($post->ID, '_igv_video_award_bio', true);
-    $minutes = get_post_meta($post->ID, '_igv_video_award_minutes', true);
-    $seconds = get_post_meta($post->ID, '_igv_video_award_seconds', true);
-        
+      $video_year = get_post_meta($post->ID, '_igv_video_award_video_year', true);
+      $bio = get_post_meta($post->ID, '_igv_video_award_bio', true);
+      $minutes = get_post_meta($post->ID, '_igv_video_award_minutes', true);
+      $seconds = get_post_meta($post->ID, '_igv_video_award_seconds', true);
+
 ?>
       <div class="information-overlay grid-item item-m-4">
         <h2 class="font-uppercase font-size-large"><?php the_title(); ?></h2>
@@ -33,14 +33,14 @@ if (have_posts()) {
 
       <div class="grid-item item-s-12 background-cover font-uppercase" style="background-image: url(<?php echo the_post_thumbnail_url('full'); ?>)">
         <h2><?php the_title(); ?></h2>
-        <div><?php echo $artists; ?></div> 
+        <div><?php echo $artists; ?></div>
         <div><?php echo get_the_date('F Y'); ?></div>
         <div class="font-uppercase">information</div>
       </div>
-  
+
 <?php
 } else { //grid post
-?> 
+?>
 
       <div class="grid-item item-s-4 background-cover font-uppercase" style="background-image: url(<?php echo the_post_thumbnail_url('full'); ?>)">
         <h2><?php the_title(); ?></h2>
@@ -48,7 +48,7 @@ if (have_posts()) {
         <div><?php echo get_the_date('F Y'); ?></div>
       </div>
 
-<?php 
+<?php
     } //end post condition
   } //endwhile
 } // endif
