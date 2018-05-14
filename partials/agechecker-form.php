7a43d62d55235ab2630ad885c93f3cc8663a1d73
mikeyrayvon <mikey@interglobal.vision>
2018-05-14 18:34:45 -0500

    add email input to age check form

--- partials/agechecker-form.php
+++ partials/agechecker-form.php
@@ -1,24 +1,30 @@
 <div id="age-overlay" class="grid-row justify-center">
 
   <form id="age-checker-form" class="grid-column align-items-center justify-center grid-item item-s-10 item-m-6 item-l-4">
-    
+
     <div class="padding-bottom-basic">
       <?php get_template_part('partials/logo') ?></a>
     </div>
 
+    <p>Enter your email address to recieve updates:</p>
+
+    <div class="grid-row">
+      <input id="mailchimp-email" type="email" placeholder="EMAIL" name="EMAIL" class="item-s-12">
+    </div>
+
     <p>Please enter your date of birth:<p>
-    
+
     <div class="grid-row justify-center padding-bottom-small">
       <div>
         <input id="birthday-month" name="month" value="" type="number" min="1" max="12" placeholder="MM">
       </div>
 
       <div>
-         <input id="birthday-day" name="day" value="" type="number" min="1" max="31" placeholder="DD">
+        <input id="birthday-day" name="day" value="" type="number" min="1" max="31" placeholder="DD">
       </div>
 
       <div>
-         <input id="birthday-year" name="year" value="" type="number" min="1900" max="<?php echo date("Y"); ?>"placeholder="YYYY">
+        <input id="birthday-year" name="year" value="" type="number" min="1900" max="<?php echo date("Y"); ?>"placeholder="YYYY">
       </div>
     </div>
 
@@ -29,5 +35,3 @@
   </form>
 
 </div>
-
-
