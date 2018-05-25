d975310a75f538ba8cd380ebf82ab179a6c2b836
mikeyrayvon <mikey@interglobal.vision>
2018-05-25 14:07:55 -0500

    add cart background toggle

--- partials/cart.php
+++ partials/cart.php
@@ -1,22 +1,25 @@
 <div id="cart">
-  <div class="grid-row padding-top-mid padding-bottom-basic">
-    <div class="grid-item flex-grow">
-      <h2 class="font-uppercase font-size-large font-bold font-outline">Bag</h2>
+  <div id="cart-background-toggle" class="js-cart-toggle"></div>
+  <div id="cart-content" class="padding-bottom-large">
+    <div class="grid-row padding-top-mid padding-bottom-basic">
+      <div class="grid-item flex-grow">
+        <h2 class="font-uppercase font-size-large font-bold font-outline">Bag</h2>
+      </div>
+
+      <div class="grid-item">
+        <div id="cart-close" class="js-cart-toggle"></div>
+      </div>
     </div>
 
-    <div class="grid-item">
-      <div id="cart-close" class="js-cart-toggle"></div>
+    <div class="grid-row">
+
+      <div id="items-container"></div>
+
+      <div class="grid-item item-s-12 item-m-4 offset-m-8">
+        <div id="subtotal-container"></div>
+        <div id="checkout-container"></div>
+      </div>
     </div>
+
   </div>
-
-  <div class="grid-row">
-
-    <div id="items-container"></div>
-
-    <div class="grid-item item-s-12 item-m-4 offset-m-8">
-      <div id="subtotal-container"></div>
-      <div id="checkout-container"></div>
-    </div>
-  </div>
-
 </div>
