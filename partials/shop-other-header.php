5cd5a6a6edba17a300d00210b4e0802be17df27c
mikeyrayvon <mikey@interglobal.vision>
2018-05-25 14:46:21 -0500

    fix shop-headers layout

--- partials/shop-other-header.php
+++ partials/shop-other-header.php
@@ -1,9 +1,9 @@
 <div class="grid-row padding-top-mid padding-bottom-basic">
-  <div class="grid-item item-s-6 item-m-8">
+  <div class="grid-item item-s-9 item-m-8">
     <h2 class="font-uppercase font-size-large font-bold font-outline">Other stuff</h2>
   </div>
 
-  <div class="grid-item item-s-6 item-m-4 padding-top-tiny">
+  <div class="grid-item item-s-3 item-m-4 padding-top-micro">
     <button class="js-cart-toggle font-uppercase">Bag ( <span id="cart-counter">0</span> )</button>
   </div>
 </div>
