10cd184c62422a86947e9c9c7fecc83440f09b93
mikeyrayvon <mikey@interglobal.vision>
2018-05-25 13:00:48 -0500

    improve cart close position. set pointer cursor for carousel

--- partials/cart.php
+++ partials/cart.php
@@ -1,8 +1,15 @@
 <div id="cart">
-  <div class="grid-row">
-    <div class="grid-item item-s-12 padding-top-mid padding-bottom-basic">
+  <div class="grid-row padding-top-mid padding-bottom-basic">
+    <div class="grid-item flex-grow">
       <h2 class="font-uppercase font-size-large font-bold font-outline">Bag</h2>
     </div>
+
+    <div class="grid-item">
+      <div id="cart-close" class="js-cart-toggle"></div>
+    </div>
+  </div>
+
+  <div class="grid-row">
 
     <div id="items-container"></div>
 
@@ -11,5 +18,5 @@
       <div id="checkout-container"></div>
     </div>
   </div>
-  <div id="cart-close" class="js-cart-toggle"></div>
+
 </div>
